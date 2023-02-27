d6610837fb3b460ec048e18551d4b592b9b737fd
RajanBakshi238 <rajan.bakshi238@gmail.com>
2023-02-27 13:23:58 +0530

    react-router-dom initialized

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element ={<>hiiii</>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
