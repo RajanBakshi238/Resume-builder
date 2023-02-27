19957f7ec0b25f0b44f1a23531cfe5be5a7343e5
RajanBakshi238 <rajan.bakshi238@gmail.com>
2023-02-27 14:44:11 +0530

    error and loading page intialized

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import Home from "./components/Home";
+import Resume from "./components/Resume";
+
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element ={<>hiiii</>} />
+          <Route path="/" element ={<Home />} />
+          <Route path="/resume" element={<Resume />} />
         </Routes>
       </BrowserRouter>
     </div>
