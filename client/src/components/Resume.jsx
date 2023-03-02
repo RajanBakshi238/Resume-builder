c5783692885ec5c0f3a1820d92ee19f5e874a2b4
RajanBakshi238 <rajan.bakshi238@gmail.com>
2023-03-02 15:53:42 +0530

    resume page done

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import ErrorPage from "./ErrorPage";
 
 const Resume = ({ result }) => {
+  // replace new line with br tag
+  const replaceWithBr = (string) => {
+    return string.replace(/\n/g, "<br />");
+  };
+
+  // error page if the result object is empty.
   if (JSON.stringify(result) === "{}") {
     return <ErrorPage />;
   }
@@ -12,7 +18,62 @@ const Resume = ({ result }) => {
     <>
       <button onClick={handlePrint}>Print Page</button>
       <main className="container">
-        <p>Hello!</p>
+        <header className="header">
+          <div>
+            <h1>{result.fullName}</h1>
+            <p className="resumeTitle headerTitle">
+              {result.currentPosition} ({result.currentTechnologies})
+            </p>
+            <p className="resumeTitle">
+              {result.currentLength}year(s) work experience
+            </p>
+          </div>
+          <div>
+            <img
+              src={result.image_url}
+              alt={result.fullName}
+              className="resumeImage"
+            />
+          </div>
+        </header>
+        <div className="resumeBody">
+          <div>
+            <h2 className="resumeBodyTitle">PROFILE SUMMARY</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: replaceWithBr(result.objective),
+              }}
+              className="resumeBodyContent"
+            />
+          </div>
+          <div>
+            <h2 className="resumeBodyTitle">WORK HISTORY</h2>
+            {result.workHistory.map((work) => (
+              <p className="resumeBodyContent" key={work.name}>
+                <span style={{ fontWeight: "bold" }}>{work.name}</span> -{" "}
+                {work.position}
+              </p>
+            ))}
+          </div>
+          <div>
+            <h2 className="resumeBodyTitle">JOB PROFILE</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: replaceWithBr(result.jobResponsibilities),
+              }}
+              className="resumeBodyContent"
+            />
+          </div>
+          <div>
+            <h2 className="resumeBodyTitle">JOB RESPONSIBILITIES</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: replaceWithBr(result.keypoints),
+              }}
+              className="resumeBodyContent"
+            />
+          </div>
+        </div>
       </main>
     </>
   );
