62a5557623d105ea43f9821a4faef8369ee091bd
RajanBakshi238 <rajan.bakshi238@gmail.com>
2023-03-02 14:30:05 +0530

    open ai implemented at the backend

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const configuration = new Configuration({
   apiKey: "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",
 });
 
-const openai = new OpenAIApi(configuration); 
+const openai = new OpenAIApi(configuration);
 
 const GPTFunction = async (text) => {
   const response = await openai.createCompletion({
@@ -22,11 +22,10 @@ const GPTFunction = async (text) => {
     top_p: 1,
     frequency_penalty: 1,
     presence_penalty: 1,
-  })
+  });
 
   return response.data.choices[0].text;
-}
-
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -63,11 +62,60 @@ app.post("/resume/create", upload.single("headshotImage"), async (req, res) => {
     workHistory, // JSON format
   } = req.body;
 
+  const workArray = JSON.parse(workHistory); //an array
+
+  let database = []
+
+  //group the values into an object
+
+  const newEntry = {
+    id: generateID(),
+    fullName,
+    image_url: `http://localhost:4004/uploads/${req.file.filename}`,
+    currentPosition,
+    currentLength,
+    currentTechnologies,
+    workHistory: workArray,
+  };
+
   console.log(req.body);
+
+  // loops through the items in the workArray and converts them to a string
+
+  const remainderText = () => {
+    let stringText = "";
+    for (let i = 0; i < workArray.length; i++) {
+      stringText += `${workArray[i].name} as a ${workArray[i].position}.`;
+    }
+
+    return stringText;
+  };
+
+  // The job description prompt
+  const prompt1 = `I am writing a resume, my details are \n name: ${fullName} \n role: ${currentPosition} (${currentLength} years). \n I write in the technolegies: ${currentTechnologies}. Can you write a 100 words description for the top of the resume(first person writing)? `;
+
+  // The job responsibilities prompt
+  const prompt2 = `I am writing a resume, my details are \n name: ${fullName} \n role: ${currentPosition} (${currentLength} years). \n I write in the technolegies: ${currentTechnologies}. Can you write 10 points for a resume on what I am good at?`;
+
+  // The job achievements prompt
+  const prompt3 = `I am writing a resume, my details are \n name: ${fullName} \n role: ${currentPosition} (${currentLength} years). \n During my years I worked at ${
+    workArray.length
+  } companies. ${remainderText()} \n Can you write me 50 words for each company seperated in numbers of my succession in the company (in first person)?`;
+
+  // Generate a GPT-3 result
+  const objective = await GPTFunction(prompt1);
+  const keypoints = await GPTFunction(prompt2);
+  const jobResponsibilities = await GPTFunction(prompt3);
+
+  const chatgptData = { objective, keypoints, jobResponsibilities };
+
+  const data = {...newEntry, ...chatgptData};
+  database.push(data);
+  console.log(chatgptData);
 
   res.json({
     message: "Request successful!",
-    data: {},
+    data
   });
 });
 
